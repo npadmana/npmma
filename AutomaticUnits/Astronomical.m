fe1bb38abc673c64ad1c98e346d82e43fee75592
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-04-22 20:24:56 -0400

    Add in some more useful units

--- AutomaticUnits/Astronomical.m
+++ AutomaticUnits/Astronomical.m
@@ -9,20 +9,18 @@ UnitSet["Astronomical"]={
 	DeclareUnit["LunarDistance", Unit[384403000, "Meter"], UsageMessage->"LunarDistance is a unit of length."], 
 	DeclareUnit["EarthRadius", Unit[6371000, "Meter"], UsageMessage->"EarthRadius is a unit of length.",TraditionalLabel -> SubscriptBox["R","\[CirclePlus]"]], 
 	(*Mass*)
-	DeclareUnit["SolarMass", Unit[1.998922*^33,"Gram"], UsageMessage->"SolarMass is a unit of mass."], (*http://en.wikipedia.org/wiki/Solar_Mass*)
+	DeclareUnit["SolarMass", Unit[1.988922*^33,"Gram"], UsageMessage->"SolarMass is a unit of mass."], (*http://en.wikipedia.org/wiki/Solar_Mass*)
 	DeclareUnit["EarthMass", Unit[5.9742*10^24, "Kilogram"], UsageMessage->"EarthMass is a unit of mass."], 	
 	DeclareUnit["JupiterMass", Unit[1.8986*10^27, "Kilogram"], UsageMessage->"JupiterMass is a unit of mass."],
 	(*Time*)
 	Unit["Day"],
 	DeclareUnit["JulianYear",Unit[365.25,"Day"],UsageMessage->"JulianYear is a unit of time."],
-	DeclareUnit["JulianCentury",Unit[100,"JulianYear"],UsageMessage->"JulianCentury is a unit of time.",TraditionalLabel -> "D"]
-};
-
-UnitSet["Hubble"]={
-        (*Other*)
+	DeclareUnit["JulianCentury",Unit[100,"JulianYear"],UsageMessage->"JulianCentury is a unit of time.",TraditionalLabel -> "D"],
+        (*Useful Cosmology Units*)
         DeclareUnit["HubbleConstant",Unit[100, "Kilometer"/"Second"/"Mpc"],
                     UsageMessage->"The Hubble Constant"],
         DeclareUnit["HubbleTime", Unit[1, 1/"HubbleConstant"], UsageMessage->"HubbleTime is 1/H0."],
         DeclareUnit["SpeedOfLight",Unit[299792458,"Meter"/"Second"], UsageMessage->"SpeedOfLight is the speed of light in m/s."],
-        DeclareUnit["HubbleDistance",Unit[1,"SpeedOfLight" "HubbleTime"], UsageMessage->"HubbleDistance is c/H0."]
+        DeclareUnit["HubbleDistance",Unit[1,"SpeedOfLight" "HubbleTime"], UsageMessage->"HubbleDistance is c/H0."],
+        DeclareUnit["NewtonG",Unit[6.67384*^-11, "Meter"^3 "Kilogram"^-1 "Second"^-2], UsageMessage->"NewtonG is the Newton gravitational constant in SI units"]
 };
